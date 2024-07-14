26ec1566243d9968bf1a20d50e45cd42180ac902
Dishant Bisht <dakshbisht1999@gmail.com>
2024-07-15 04:16:03 +0530

    removed service method and reading counter value from the store

--- src/app/counter-output/counter-output.component.ts
+++ src/app/counter-output/counter-output.component.ts
@@ -1,30 +1,19 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { CounterControlsComponent } from '../counter-controls/counter-controls.component';
-import { Subscription } from 'rxjs';
-import { CounterService } from '../counter.service';
+import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-counter-output',
   templateUrl: './counter-output.component.html',
   styleUrls: ['./counter-output.component.scss']
 })
-export class CounterOutputComponent implements OnInit, OnDestroy {
+export class CounterOutputComponent {
+  count$: Observable<number>;
 
-  counter: number = 0;
-  counterServiceSub?: Subscription;
-
-  constructor(private counterService: CounterService) { }
-
-  ngOnInit(): void {
-    this.counterServiceSub = this.counterService.counterChanged.subscribe((newVal: number)=>{
-      this.counter = newVal;
-    })
-  }
-
-  ngOnDestroy(): void{
-    if(this.counterServiceSub){
-      this.counterServiceSub.unsubscribe();
-    }
+  constructor(
+    private store: Store<{counter: number}> // generic type annotation to tell the ts which reducer are available to access from module.ts
+  ) {
+    this.count$ = store.select('counter'); // select is used to read the value from the store
   }
 
 }
