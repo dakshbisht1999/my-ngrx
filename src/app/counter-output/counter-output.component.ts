7d58b7054a40acb037fc1b8cdbb609acbe3c7f61
Dishant Bisht <dakshbisht1999@gmail.com>
2024-07-15 19:59:46 +0530

    Introduced selectors: Using .select() with dynamic selectors from selectors.ts file

--- src/app/counter-output/counter-output.component.ts
+++ src/app/counter-output/counter-output.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Store } from '@ngrx/store';
+import { selectCount, selectDoubleCount } from '../store/counter.selectors';
 
 @Component({
   selector: 'app-counter-output',
@@ -9,11 +10,13 @@ import { Store } from '@ngrx/store';
 })
 export class CounterOutputComponent {
   count$: Observable<number>;
+  doubleCount$: Observable<number>;
 
   constructor(
     private store: Store<{counter: number}> // generic type annotation to tell the ts which reducer are available to access from module.ts
   ) {
-    this.count$ = store.select('counter'); // select is used to read the value from the store
+    this.count$ = store.select(selectCount); // select is used to read the value from the store
+    this.doubleCount$ = store.select(selectDoubleCount);
   }
 
 }
